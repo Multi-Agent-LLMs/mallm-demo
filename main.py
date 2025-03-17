9c317865429b31c4532b4d89382e5505f988a1d3
jonas-becker <snakeeye98@web.de>
2025-03-17 16:59:29 +0100

    scheduler runs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 from itertools import product
+from mallm.scheduler import Scheduler
+from mallm.utils.config import Config
 
-def generate_configs():
+def generate_configs(endpoint_url="http://127.0.0.1:8080/v1"):
     # Load grid search parameters
     with open('gridsearch_parameters', 'r') as file:
         grid_params = json.load(file)
@@ -20,23 +22,27 @@ def generate_configs():
     # Create configuration objects
     configs = []
     for combination in combinations:
-        config = {
-            'model_name': combination[0],
-            'dataset': combination[1],
-            'response_generator': combination[2],
-            'persona_generator': combination[3],
-            'discussion_paradigm': combination[4],
-            'decision_protocol': combination[5]
-        }
+        config = Config(
+            endpoint_url = endpoint_url,
+            model_name = combination[0],
+            input_json_file_path = combination[1],
+            response_generator = combination[2],
+            agent_generator = combination[3],
+            discussion_paradigm = combination[4],
+            decision_protocol = combination[5],
+            output_json_file_path = f"out/{combination[0]}_{combination[1]}_{combination[2]}_{combination[3]}_{combination[4]}_{combination[5]}.json",
+        )
         configs.append(config)
 
     print(f"Generated {len(configs)} configurations")
-    for config in configs:
-        print(json.dumps(config, indent=2)) 
+    return configs
 
 
 def main():
-    generate_configs()
+    configs = generate_configs()
+    for config in configs:
+        mallm_scheduler = Scheduler(config)
+        mallm_scheduler.run()
 
 if __name__ == "__main__":
     main()
